1f17cd4848cb53295ff01a43b5fea32119fe2502
Leo Chung <stargameleochung@gmail.com>
2025-08-31 12:06:18 -0700

    SPLASH

--- scripts/main.js
+++ scripts/main.js
@@ -3,9 +3,49 @@ import * as display from "./display/index.js";
 
 /** Picks victims in a totally fair and unbiased way.
  *  @author git-doge (initial version) and bloopsoup */
-class Main {
+export default class Main {
+    /** Initializes the page. */
+    static init() { document.getElementById('dialog')?.focus(); }
+
+    /**
+     * Handle a keyboard enter press.
+     * @param {KeyboardEvent} e - The keyboard event.
+     */
+    static handleEnter(e) { if (e.key === 'Enter') Main.run(); }
+
+    /** Runs the page. */
+    static run() {
+        // Hide the dialog
+        const dialog = document.getElementById('dialog');
+        if (dialog) {
+            dialog.style.transition = 'opacity 4s ease';
+            dialog.style.opacity = '0';
+            dialog.style.pointerEvents = 'none';
+            dialog.addEventListener('transitionend', () => dialog.style.display = 'none', { once: true });
+        }
+
+        // Display layout elements
+        document.querySelectorAll('header').forEach(element => element.removeAttribute('style'));
+        document.querySelectorAll('main').forEach(element => element.removeAttribute('style'));
+        document.querySelectorAll('footer').forEach(element => element.removeAttribute('style'));
+
+        // Display static elements
+        const [pickedStatus, pickedImage] = [document.getElementById('picked-status'), document.getElementById('picked-image')];
+        if (pickedStatus) pickedStatus.textContent = 'victim';
+        pickedImage?.removeAttribute('hidden');
+        
+        // Play audio
+        const audio = new Audio('../audio/lullaby.mp3');
+        audio.loop = true;
+        audio.play();
+
+        // Run update once and then set an interval
+        Main.#update();
+        setInterval(() => Main.#update(), 1000);
+    }
+
     /** Updates the page. */
-    static update() {
+    static #update() {
         const nameChoices = ['cHRIS - mR oSU kING', 'walter', 'mrs. until', 'poopsicle'];
         const chosenName = Stacked.getChoice(nameChoices, Calendar.totalTodayDays);
         const names = Array.from({length: Calendar.totalElapsedDays}, (_, i) => Stacked.getChoice(nameChoices, Calendar.totalTodayDays - i));
@@ -16,13 +56,11 @@ class Main {
         const sus = ((1 - pValue) / .95) * 100;
 
         // Display the elements
-        const [pickedStatus, pickedName, pickedDate, pickedImage] = [document.getElementById('picked-status'), document.getElementById('picked-name'), document.getElementById('picked-date'), document.getElementById('picked-image')];
+        const [pickedName, pickedDate] = [document.getElementById('picked-name'), document.getElementById('picked-date')];
         const [statsRecent, statsSummaryHeader, statsSummary, statsPie] = [document.getElementById('stats-recent'), document.getElementById('stats-summary-header'), document.getElementById('stats-summary'), document.getElementById('stats-pie')];
 
-        if (pickedStatus) pickedStatus.textContent = 'victim';
         if (pickedName) pickedName.textContent = chosenName;
         if (pickedDate) pickedDate.textContent = new Date().toLocaleString();
-        pickedImage?.removeAttribute('hidden');
 
         if (statsSummaryHeader) statsSummaryHeader.textContent = `stats (TOTAL ${names.length}) (SUS ${sus.toFixed(2)}%)`
         statsRecent?.setAttribute('items', names.slice(1, 6).join(','));
@@ -30,5 +68,3 @@ class Main {
         statsPie?.setAttribute('items', names.join(','));
     }
 }
-
-setInterval(() => Main.update(), 1000);
